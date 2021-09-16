a52838f7f99881e512b6d250a2697d75380843be
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2021-09-16 19:27:25 -0400

    backing up everything ive changes

--- Week3/MVCHW/app/app.js
+++ Week3/MVCHW/app/app.js
@@ -1,10 +1,27 @@
 
 function pageNavBindings(){
     // This it he function specifcally that adds the bindings and runs comands in the model page that allows the page to update dynamically changing the content within the content div
+    $(".actionBTN").click(function(e){
+        let btnID = e.currentTarget.id;
+        $(".actionBTN").removeClass("selectedRed selectedBlue selectedPurple selectedPink")
+        console.log(btnID);
+        MODEL.changePageContent(btnID);
 
+        if(btnID == "home"){
+            $("#home").addClass("selectedRed");
+        }else if (btnID == "about"){
+            $("#about").addClass("selectedBlue");
+        } else if (btnID == "services"){
+            $("#services").addClass("selectedPurple");
+        } else {
+            $("#contact").addClass("selectedPink");
+        }
+    });
 };
 
-// This here runs code that binds listening actions to tthe page website buttons so that we can dynamically change the contents within the page when a user clicks one of the buttons. It loads at the very starts and functions throughout.
+
+
+// It loads at the very starts and functions throughout.
 // I write a console log that runs after each function letting me know if a function has succesfully ran.
 $(document).ready(function(){
     console.log("Page as begun loading and functions have beegun running. Current Functions opperating are:");
--- Week3/MVCHW/app/model.js
+++ Week3/MVCHW/app/model.js
@@ -1,14 +1,19 @@
 var MODEL = (function(){
     // here we create our hidden variables of the pages content. It gets stored here and swapped out when needed
-
+    var _homePageInfo = ``;
+    var _aboutPageInfo = ``;
+    var _servicesPageInfo = ``;
+    var _contactPageInfo = ``;
 
     // this is out hidden function that controls the page changes that occure. Here is where are var is loaded and changes when needed.
     // it does this by targeting our content div and change out the html elements within with what ever information was stored in the var file.
-    var __changePageContent = function (pageNamee){
-
+    var _changePageContent = function(pageName){
+        let newName = "__" + pageName + "PageInfo"
+        console.log(newName);
+        //$()
     }
 
     return{
-        getPageContent: __changePageContent,
+        changePageContent: _changePageContent,
     };
-});
+})();
