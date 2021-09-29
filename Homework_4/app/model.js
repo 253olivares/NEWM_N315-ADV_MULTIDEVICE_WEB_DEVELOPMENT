e2d2c99ed8c88cc0f09c944ee616c9267fc1ad08
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2021-09-29 17:35:45 -0400

    added modal code to the js file

--- Homework_4/app/model.js
+++ Homework_4/app/model.js
@@ -1,5 +1,21 @@
 var MODEL = (function(){
-    var _modalSignUp = ``;
+    var _modalSignUp = `<div class="modal">
+    <div class="bg-click"></div>
+    <div class="callout sig">
+      <img src="/images/Amazon.png" alt="Amazon Logo">
+      <label class="textLabel" for="firstname">Firstname:</label>
+      <input class="textField" type="text" id="firstname" name="firstname">
+      <label class="textLabel" for="lastword">Lastname:</label>
+      <input class="textField" type="text" id="lastnane" name="lastname">
+      <label class="textLabel" for="username">Username:</label>
+      <input class="textField" type="text" id="username" name="username">
+      <label class="textLabel" for="password">Password:</label>
+      <input class="textField" type="text" id="password" name="password">
+      <label class="textLabel" for="email">Email:</label>
+      <input class="textField" type="text" id="email" name="email">
+      <input class="signButton styling" type="submit" value="Signup">
+    </div>
+  </div>`;
     var _modalLogin = ` <div class="modal">
     <div class="bg-click"></div>
     <div class="callout log">
