b46f8636ca3005c57fef92fd06fe31dc6c4a0213
Miguel Olivares <67510851+253olivares@users.noreply.github.com>
2021-10-19 01:21:50 -0400

    added hamburger menu

--- Homework_6/app/app.js
+++ Homework_6/app/app.js
@@ -7,7 +7,31 @@ function route(){
         MODEL.changeContent(pageID);
     }
 }
-
+function checkMenu(){
+    let menu = "close";
+    $(".mobile-menu").click(function(e){
+        if(menu == "close"){
+            $(".hiddenNav").css("top","0");
+            $(".mobile-menu").css("display","none");
+            menu = "open";
+        } else {
+            $(".hiddenNav").css("top","-100%");
+            $(".mobile-menu").css("display","flex");
+            menu = "close";
+        }
+    });
+    $(".removeButton").click(function(e){
+        if(menu == "close"){
+            $(".hiddenNav").css("top","0");
+            $(".mobile-menu").css("display","none");
+            menu = "open";
+        } else {
+            $(".hiddenNav").css("top","-100%");
+            $(".mobile-menu").css("display","flex");
+            menu = "close";
+        }
+    });
+}
 function checkHash(){
     $(window).on("hashchange",route);
     route();
@@ -16,4 +40,5 @@ function checkHash(){
 $(document).ready(function(){
     console.log("Page functions have loaded! checkHash() function has begun running!");
     checkHash();
+    checkMenu();
 });
--- Homework_6/app/model.js
+++ Homework_6/app/model.js
@@ -7,14 +7,18 @@ var MODEL = (function(){
             console.log(butnPlace);
             if(page == "home"){
                 $(".navMen").removeClass("selected");
+                $(".mobileLink").removeClass("aSelected");
                 $("#page-container").css("background-image", "linear-gradient(to right, rgba(242,92,84,.6),rgba(242,92,84,.6)), url(../images/hero.jpg)");
                 $("#page-container").css("background-color", "none");
                 $("."+butnPlace).addClass("selected");
+                $(".homeMobile").addClass("aSelected");
                 $('#app').html(data);
             } else if (page == "login") {
                 $(".navMen").removeClass("selected");
+                $(".mobileLink").removeClass("aSelected");
                 $("#page-container").css("background-image", "none");
                 $("#page-container").css("background-color", "#FFD972");
+                $(".loginMobile").addClass("aSelected");
                 $('#app').html(data);
             } else {
                 $('#app').html(data);
